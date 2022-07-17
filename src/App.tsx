8d7ceadb68acad3dabd31d7f14744d2ea30bd36d
Svibex <future.science@yandex.ru>
2022-07-17 13:07:58 +0300

    feat: complete mainPage description

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,14 @@ import React from 'react';
 import './styles/reset.css';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import MainPage from "./components/MainPage/MainPage";
 
 function App() {
-    const divStyle = {
-        background: 'blue',
-        height: '500px',
-    };
 
   return (
     <>
       <Header />
-        <div style={divStyle} />
+      <MainPage />
       <Footer />
     </>
   );
