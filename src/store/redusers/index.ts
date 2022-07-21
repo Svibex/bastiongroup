cfd6b6a7e25d90816730efb81c96b78058da6f96
Svibex <future.science@yandex.ru>
2022-07-21 20:43:47 +0300

    feat: add gh-pages deploy

--- src/store/redusers/index.ts
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux'
-import {productReducer} from "./productReducer";
-
-export const rootReducer = combineReducers(({
-    product: productReducer,
-}))
-
-export type RootState = ReturnType<typeof rootReducer>
